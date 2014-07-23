03ceea9551a898bf499e5708c269f588c728b28b
Khang Nguyen <nguyenkhang90@gmail.com>
2014-07-23 09:23:47 +0000

    Add config files

--- application.py
+++ application.py
@@ -2,12 +2,24 @@ from flask import Flask
 from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app.config.from_envvar('GTOKEN_COMMON_SETTINGS')
+app.config.from_envvar('GTOKEN_LOCAL_SETTINGS')
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
     app.run()
+
+if not app.debug:
+    import logging
+    from logging.handlers import SMTPHandler
+    mail_handler = SMTPHandler('127.0.0.1',
+                               'admin@gtoken.com',
+                               app.config['ADMINS'], 'GToken server had an error')
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
---
+++ config/common.py
@@ -0,0 +1,4 @@
+DATEFORMAT =  "%Y-%m-%d"
+DATETIMEFORMAT = "%Y-%m-%d %H:%M"
+ARCHIVEDATEFORMAT = "%a %b %d, %Y"
+FULLDATETIMEFORMAT = "%Y-%m-%d %H:%M:%S %Z"
